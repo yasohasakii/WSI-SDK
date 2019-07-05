e43fa4ae48b760f34947502496916bea888e0042
JumTsai <jumtsai@foxmail.com>
2019-07-05 17:14:12 +0800

    fix bugs

--- wsiread.py
+++ wsiread.py
@@ -6,10 +6,10 @@ import openslide
 import numpy as np
 
 class WSI(object):
-	def __init__(self,svs_path):
-		self.path = svs_path
-		self.wsi = openslide.OpenSlide(self.path)
-		self.header = {}
+    def __init__(self,svs_path):
+	    self.path = svs_path
+	    self.wsi = openslide.OpenSlide(self.path)
+        self.header = {}
 		self.image = None
         self.header['producer'] = self.wsi.detect_format(self.path)
         self.header['level_count'] = self.wsi.level_count
@@ -28,15 +28,7 @@ class WSI(object):
                 savepath = os.path.join(folder,savepath.split('/')[-1])
         with open(savepath,'wb') as header:
             pickle.dump(self.header,header,pickle.HIGHEST_PROTOCOL)
-
-    def function():
-        pass
-
-
-        
-
-
-
+            
     def read(self, level = 0, patch_size = 4000):
         """
         :param level:
@@ -44,7 +36,7 @@ class WSI(object):
         :return:
         """
         [x,y] =self.wsi.level_dimensions[level]
-        print("Image size: {} {}".format(x,y))
+        print("Image size of level {}: {} {}".format(level, x, y))
         image = np.zeros([x,y,3],np.uint8)
         x_range = list(range(0,x,patch_size))
         y_range = list(range(0,y,patch_size))
@@ -59,10 +51,9 @@ class WSI(object):
                 patch = np.array(self.wsi.read_region((i ,j) ,downsampling ,[x_size ,y_size]))
                 if patch.shape[2]==4:
                     r,g,b,a = cv2.split(patch)
-                    patch = cv2.merge([r,g,b,a])
-
-
-
+                    patch = cv2.merge([r,g,b])
+                image[j:j+y_size,x:x+x_size] = patch
+        return image
 
 # if __name__=="__main__":
 #     readimg("TCGA.svs")
