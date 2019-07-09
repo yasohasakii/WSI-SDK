0551903563432336fe5a7d50164bc6dcf08616ac
Jum <jumtsai@foxmail.com>
2019-07-09 15:34:07 +0800

    Update wsi.py

--- wsi.py
+++ wsi.py
@@ -7,10 +7,10 @@ import numpy as np
 
 class WSI(object):
     def __init__(self,svs_path):
-	    self.path = svs_path
-	    self.wsi = openslide.OpenSlide(self.path)
+        self.path = svs_path
+        self.wsi = openslide.OpenSlide(self.path)
         self.header = {}
-		self.image = None
+        self.image = None
         self.header['producer'] = self.wsi.detect_format(self.path)
         self.header['level_count'] = self.wsi.level_count
         self.header['level_downsamples'] = self.wsi.level_downsamples
@@ -37,7 +37,7 @@ class WSI(object):
         """
         [x,y] =self.wsi.level_dimensions[level]
         print("Image size of level {}: {} {}".format(level, x, y))
-        image = np.zeros([x,y,3],np.uint8)
+        image = np.zeros([y,x,3],np.uint8)
         x_range = list(range(0,x,patch_size))
         y_range = list(range(0,y,patch_size))
         for i in x_range:
@@ -48,11 +48,11 @@ class WSI(object):
                     x_size = x-i
                 else:
                     y_size = y-j
-                patch = np.array(self.wsi.read_region((i ,j) ,downsampling ,[x_size ,y_size]))
+                patch = np.array(self.wsi.read_region((i ,j) ,level ,[x_size ,y_size]))
                 if patch.shape[2]==4:
                     r,g,b,a = cv2.split(patch)
                     patch = cv2.merge([r,g,b])
-                image[j:j+y_size,x:x+x_size] = patch
+                image[j:j+y_size,i:i+x_size] = patch
         return image
 
 # if __name__=="__main__":
